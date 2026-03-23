99f92ce73b72c48c57976a4a07fefb602f73baef
Heidi Suurkaulio <heidisuur@gmail.com>
2026-03-23 15:24:58 +0200

    example and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Brown Clustering with Finnish text data
+
+A repository for applying [Brown Clustering](../helpmefindaname/BrownClustering) algorithm on Finnish text.
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from brown_clustering import BigramCorpus, BrownClustering
+
 def main():
-    print("Hello from bronwcluster!")
+    # use some tokenized and preprocessed data
+    sentences = [
+        ["This", "is", "an", "example"],
+        ["This", "is", "another", "example"]
+    ]
+
+    # create a corpus
+    corpus = BigramCorpus(sentences, alpha=0.5, min_count=0)
+
+    # (optional) print corpus statistics:
+    corpus.print_stats()
+
+    # create a clustering
+    clustering = BrownClustering(corpus, m=4)
+
+    # train the clustering
+    clusters = clustering.train()
 
 
 if __name__ == "__main__":
